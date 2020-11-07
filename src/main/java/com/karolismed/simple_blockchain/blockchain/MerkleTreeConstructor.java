932bdfc9e42d4264bd4b85f3bae38f58544d1e72
Karolis Medekša <karolis.medeksa@devbridge.com>
2020-11-07 15:33:01 +0200

    Improve merkle tree creation algorithm

--- src/main/java/com/karolismed/simple_blockchain/blockchain/MerkleTreeConstructor.java
+++ src/main/java/com/karolismed/simple_blockchain/blockchain/MerkleTreeConstructor.java
@@ -2,6 +2,7 @@ package com.karolismed.simple_blockchain.blockchain;
 
 import com.karolismed.simple_blockchain.blockchain.model.transaction.Transaction;
 import com.karolismed.simple_blockchain.hashing.HashingService;
+import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,18 +16,26 @@ public class MerkleTreeConstructor {
         this.hashingService = new HashingService();
     }
 
-    public String getMerkleTreeRoot(List<Transaction> transactions) {
+    public String getMerkleTreeRoot(@NonNull List<Transaction> transactions) {
         List<String> currentLevel =
             transactions.stream().map(Transaction::getTxId).collect(Collectors.toList());
 
+        if (currentLevel.isEmpty()) {
+            return hashingService.hash("");
+        }
+
+        currentLevel = currentLevel.stream()
+            .map(txId -> hashingService.hash(txId))
+            .collect(Collectors.toList());
+
         while (currentLevel.size() > 1) {
             List<String> temp = new ArrayList<>();
+            if (currentLevel.size() % 2 != 0) {
+                currentLevel.add(currentLevel.get(currentLevel.size() - 1));
+            }
+
             for (int i = 0; i < currentLevel.size(); i += 2) {
-                if (i == currentLevel.size() - 1) {
-                    temp.add(hashingService.hash(currentLevel.get(i)));
-                } else {
-                    temp.add(hashingService.hash(currentLevel.get(i) + currentLevel.get(i + 1)));
-                }
+                temp.add(hashingService.hash(currentLevel.get(i) + currentLevel.get(i + 1)));
             }
             currentLevel = temp;
         }
